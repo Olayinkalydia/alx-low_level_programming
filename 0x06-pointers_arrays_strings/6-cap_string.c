805b457f68ab5b593d304e27cec572c053cabb75
olayinkalydia <adebayoolayinka940@gmail.com>
2022-10-27 13:22:09 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,8 +11,8 @@ char *cap_string(char *)
 
 	while (str[i])
 	{
-		while (!(str[i] >='a' && str[i] <= 'z'
-	))
+		while (!(str[i] > = 'a' && str[i] <= 'z'
+))
 			i++;
 		if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
